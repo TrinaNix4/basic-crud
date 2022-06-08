044c98ae536d6f150e63b3fc78f5a69f767e0e54
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-06-07 20:56:16 -0600

    added show method

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -4,4 +4,13 @@ class Api::ItemsController < ApplicationController
   def index
     render json: Item.all
   end
+
+  def show 
+    puts "params"
+    puts params
+    item = Item.find(params[:id])
+    render json: item 
+  
+  end
+
 end
